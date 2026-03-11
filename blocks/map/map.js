6b6a1511b43b2c7149b6df850af10553eb36dbb1
S <2428005@cognizant.com>
2026-03-11 22:11:32 +0530

    map iframe blocks1

--- blocks/map/map.js
+++ blocks/map/map.js
@@ -1,16 +1,14 @@
 export default function decorate(block) {
-  const url = block.querySelector('a')?.href;
-
-  if (!url) return;
+  const link = block.querySelector('a');
+  if (!link) return;
 
   const iframe = document.createElement('iframe');
-  iframe.src = url;
+  iframe.src = link.href;
   iframe.loading = 'lazy';
   iframe.style.border = '0';
-  iframe.setAttribute('allowfullscreen', true);
-  iframe.referrerPolicy = "no-referrer-when-downgrade";
+  iframe.allowFullscreen = true;
+  iframe.referrerPolicy = 'no-referrer-when-downgrade';
 
-  // Clear block content
   block.innerHTML = '';
   block.appendChild(iframe);
 }

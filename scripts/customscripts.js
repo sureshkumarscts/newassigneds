b4d5960f671452b9f9a48d6b8f8b037ccbcdadff
S <2428005@cognizant.com>
2026-03-11 16:04:21 +0530

    nav top header

--- scripts/customscripts.js
+++ scripts/customscripts.js
@@ -12,3 +12,20 @@ import {
   loadCSS,
 } from './aem.js';
 
+/**
+ * Moves an element (`moveSelector`) to be the first child of `parentSelector`.
+ * @returns {boolean} true if moved, false otherwise.
+ */
+
+function moveAsFirstChild(parentSelector, moveSelector) {
+  const parent = document.querySelector(parentSelector);
+  const toMove = document.querySelector(moveSelector);
+  if (!parent || !toMove) return false;
+  parent.prepend(toMove);
+  return true;
+}
+
+//usage
+document.addEventListener('DOMContentLoaded', () => {
+  moveAsFirstChild('.header-wrapper', '.top-nav);
+});
